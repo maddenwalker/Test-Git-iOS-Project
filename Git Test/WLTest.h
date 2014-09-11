ee3b7357e969a578a54014b407ef0f36ed1d20a9
Ryan Walker <ryan@maddenwalker.com>
2014-09-11 15:30:25 -0700

    second commit

---
+++ Git Test/WLTest.h
@@ -0,0 +1,13 @@
+//
+//  WLTest.h
+//  Git Test
+//
+//  Created by Ryan Walker on 9/11/14.
+//  Copyright (c) 2014 Witt Labs, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WLTest : NSObject
+
+@end
---
+++ Git Test/WLTest.m
@@ -0,0 +1,13 @@
+//
+//  WLTest.m
+//  Git Test
+//
+//  Created by Ryan Walker on 9/11/14.
+//  Copyright (c) 2014 Witt Labs, LLC. All rights reserved.
+//
+
+#import "WLTest.h"
+
+@implementation WLTest
+
+@end
