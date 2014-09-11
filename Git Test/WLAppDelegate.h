9ef2530f0b314087429bf9b6b7f51c7a305c714f
Ryan Walker <ryan@maddenwalker.com>
2014-09-11 15:20:58 -0700

    Initial Commit

---
+++ Git Test/WLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WLAppDelegate.h
+//  Git Test
+//
+//  Created by Ryan Walker on 9/11/14.
+//  Copyright (c) 2014 Witt Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
